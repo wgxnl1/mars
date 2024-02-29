30877cb84b8f0f0d16b9789ca20cfb4576dd4340
Egot66543 <rybakow.eg@yandex.ru>
2024-02-29 18:17:17 +0300

    pupu

--- sr3.py
+++ sr3.py
@@ -11,25 +11,17 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
-    user = User()
-    user.surname = "Scott"
-    user.name = "Ridley"
-    user.age = 21
-    user.position = "captain"
-    user.speciality = "research engineer"
-    user.address = "module_1"
-    user.email = "scott_chief@mars.org"
-    user.hashed_password = "cap"
-    user.team_leader = 1
-    user.job = 'deployment of residential modules 1 and 2'
-    user.work_size = 15
-    user.collaborators = '2,3'
-    user.start_date = datetime.datetime.now
-    user.is_finished = False
+    jobs = Jobs()
+    jobs.team_leader = 1
+    jobs.job = 'deployment of residential modules 1 and 2'
+    jobs.work_size = 15
+    jobs.collaborators = '2,3'
+    jobs.start_date = datetime.datetime.now()
+    jobs.is_finished = False
 
 
     session = db_session.create_session()
-    session.add(user)
+    session.add(jobs)
     session.commit()
     app.run()
 
